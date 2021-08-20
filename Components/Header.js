4d80758189a0efc61ede7322946207ebe282f8dc
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-08-20 21:53:19 +0530

    main

--- Components/Header.js
+++ Components/Header.js
@@ -18,17 +18,23 @@ function Header(props) {
           src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
           alt="Facebook icon"
         />
-        <div className="flex flex-row">
+        {/*Menu button*/}
+        <div className="lg:hidden">
+          <div className="w-11 h-11 mt-1.5 rounded-full bg-gray-200">
+            <SearchIcon className="w-7 h-7 relative top-2 left-2 text-gray-500" />
+          </div>
+        </div>
+        <div className="hidden lg:flex lg:flex-row">
           <SearchIcon className="w-5 h-5 relative top-1/3 left-2 text-gray-500" />
           <input
-            className="border-2 rounded-full h-10 w-60 mt-2 -ml-6 pl-9 pb-1 bg-gray-100 focus:outline-none"
+            className="border-2 rounded-full h-11 w-60 mt-2 -ml-6 pl-9 pb-1 bg-gray-100 focus:outline-none"
             placeholder="Search on Facebook"
           />
         </div>
       </div>
       {/* Center Header */}
-      <div className="flex space-x-5 mt-2">
-        <div className="flex h-12 w-24 focus-within:ring-2 hover:bg-gray-200 hover:rounded-t-md justify-center items-center">
+      <div className="hidden lg:flex lg:mt-2">
+        <div className="flex h-12 w-24 hover:bg-gray-200 hover:rounded-t-md justify-center items-center">
           <HomeIcon className="h-8 text-gray-500" />
         </div>
         <div className="flex h-12 w-24 hover:bg-gray-200 hover:rounded-t-md justify-center items-center">
@@ -46,11 +52,11 @@ function Header(props) {
       </div>
       {/*Header Right*/}
       <div className="flex space-x-5">
-        <div className="w-10 h-10 rounded-full bg-red-200" />
-        <div className="w-10 h-10 rounded-full bg-red-200" />
-        <div className="w-10 h-10 rounded-full bg-red-200" />
-        <div className="w-10 h-10 rounded-full bg-red-200" />
-        <div className="w-10 h-10 rounded-full bg-red-200" />
+        <div className="w-11 h-11 rounded-full bg-red-200" />
+        <div className="w-11 h-11 rounded-full bg-red-200" />
+        <div className="w-11 h-11 rounded-full bg-red-200" />
+        <div className="w-11 h-11 rounded-full bg-red-200" />
+        <div className="w-11 h-11 rounded-full bg-red-200" />
       </div>
     </div>
   );
