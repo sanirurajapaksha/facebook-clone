d67abc184a5efe0430dff37c4eb607d764e6a49f
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-08-22 21:54:50 +0530

    mian

--- Components/Stories.js
+++ Components/Stories.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Stories() {
-  return <div className="flex bg-red-200 h-14 w-full" />;
+  return (
+    <div className="flex bg-red-200 h-14 w-full justify-center items-center">
+      <div className="flex bg-green-400 h-9 w-20"></div>
+    </div>
+  );
 }
 
 export default Stories;
