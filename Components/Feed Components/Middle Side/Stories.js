0e5279459decd33ceef95875b31e03eae75370b7
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-08-25 16:14:04 +0530

    main

--- Components/Feed Components/Middle Side/Stories.js
+++ Components/Feed Components/Middle Side/Stories.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 function Stories() {
+  function windowSize() {
+    if (window.screen.width > 400) {
+      console.log(window.screen.width);
+    }
+  }
+
   return (
-    <div className="flex flex-row bg-red-200 h-60 w-full justify-center items-center"></div>
+    <div className="flex flex-row bg-red-200 h-60 w-full lg:justify-center">
+      <div className="w-28 h-48 bg-blue-200 my-6 mx-1"></div>
+      <div className="w-28 h-48 bg-blue-200 my-6 mx-1"></div>
+      <div className="w-28 h-48 bg-blue-200 my-6 mx-1"></div>
+      <div className="w-28 h-48 bg-blue-200 my-6 mx-1"></div>
+      {windowSize === true ? (
+        <div className="w-28 h-48 bg-blue-200 my-6 mx-1"></div>
+      ) : null}
+    </div>
   );
 }
 
