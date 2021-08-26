f6c2a03c7332229c010838212e0db7f07f89cff3
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-08-27 03:10:13 +0530

    main

--- Components/Feed Components/Middle Side/Rooms.js
+++ Components/Feed Components/Middle Side/Rooms.js
@@ -3,7 +3,9 @@ import React from "react";
 function Rooms(props) {
   return (
     <div className="flex w-full h-20 justify-center items-center">
-      <div className="h-16 w-10/12 mx-20 bg-white rounded-lg shadow-md border-2"></div>
+      <div className="h-16 w-10/12 mx-20 rounded-lg shadow-md border-2">
+        <div className="h-10 w-6 rounded-full"></div>
+      </div>
     </div>
   );
 }
--- Components/Feed Components/Middle Side/Upload.js
+++ Components/Feed Components/Middle Side/Upload.js
@@ -3,7 +3,21 @@ import React from "react";
 function Upload() {
   return (
     <div className="flex h-36 w-full justify-center items-center">
-      <div className="flex flex-row w-10/12 h-32 mx-20 rounded-lg shadow-md border-2 bg-white"></div>
+      <div className="flex flex-col divide-y-2 w-10/12 h-32 mx-20 rounded-lg shadow-md border-2">
+        <div className="w-full">
+          <div className="flex flex-row justify-center items-center h-16">
+            <div className="w-10 h-10 rounded-full bg-gray-600 mr-4"></div>
+            <div className="w-10/12 h-10 rounded-full bg-gray-300"></div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="flex flex-row h-16 mt-2 justify-evenly">
+            <div className="w-1/4 rounded-full h-10 bg-gray-300"></div>
+            <div className="w-1/4 rounded-full h-10 bg-gray-300"></div>
+            <div className="w-1/4 rounded-full h-10 bg-gray-300"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
