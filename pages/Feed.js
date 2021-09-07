38151ced2a5185968578f16b37f4c656db6da685
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-07 21:23:54 +0530

    main

--- pages/Feed.js
+++ pages/Feed.js
@@ -4,7 +4,7 @@ import Rooms from "../Components/Feed Components/Middle Side/Rooms";
 import Stories from "../Components/Feed Components/Middle Side/Stories";
 import Upload from "../Components/Feed Components/Middle Side/Upload";
 
-function Feed(props) {
+function Feed() {
   return (
     <div className="flex flex-row h-screen">
       <div className="w-1/2">
