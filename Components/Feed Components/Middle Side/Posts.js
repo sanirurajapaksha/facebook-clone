68edc267bdcd3538eb92bb6c529af9946cae34a2
Saniru Rajapaksha <sanirurajapaksha456@gmail.com>
2021-09-07 23:05:34 +0530

    main

--- Components/Feed Components/Middle Side/Posts.js
+++ Components/Feed Components/Middle Side/Posts.js
@@ -6,7 +6,7 @@ function Posts(props) {
       <div className="flex flex-col h-32 w-10/12 mx-20 rounded-lg shadow-md border-2">
         <div className="flex flex-row w-full h-12 bg-red-200 justify-between items-center">
           <div className="w-10 h-10 ml-4 rounded-full bg-gray-400"></div>
-          <div className="absolute ml-16 w-80 h-10 bg-green-300"></div>
+          <div className="flex flex-row absolute ml-16 w-80 h-10 bg-green-300"></div>
           <div className="w-10 h-10 mr-4 bg-green-300"></div>
         </div>
         <div className="flex flex-row w-full h-12 bg-blue-300 justify-start items-center"></div>
